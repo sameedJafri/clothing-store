5db8d79aafacb165096b1795ef7423b78ea7393a
Sameed <sameedjafri@hotmail.com>
2024-12-18 18:51:06 -0500

    navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,61 @@
+// TO-DO add each image for the assets objects 
+
+/*
+import p_img1 from './p_img1.jpg'
+import p_img2 from './p_img2.jpg'
+import p_img2_1 from './p_img2_1.jpg'
+import p_img3 from './p_img3.jpg'
+import p_img4 from './p_img4.jpg'
+import p_img5 from './p_img5.jpg'
+import p_img6 from './p_img6.jpg'
+*/
+
+import logo from './logo.png'
+import profile_icon from './profile_icon.png'
+import search_icon from './search_icon.png'
+import menu_icon from './menu_icon.png'
+import cart_icon from './cart_icon.png'
+
+/*
+import hero_img from './hero_img.jpg'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import quality_icon from './quality_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_icon from './support_icon.png'
+import about_icon from './about_icon.png'
+import contact_icon from './contact_icon.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+
+*/
+
+export const assets = {
+    logo,
+    //hero_img,
+    cart_icon,
+    profile_icon,
+    search_icon,
+    menu_icon,
+   /* bin_icon,
+    dropdown_icon,
+    exchange_icon,
+    quality_icon,
+    star_dull_icon,
+    star_icon,
+    support_icon,
+    about_icon,
+    contact_icon,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon */ 
+}
+
+
+
+
+
+
